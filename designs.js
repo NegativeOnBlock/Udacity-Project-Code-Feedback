52ca0ec1b70ee8cbd8043dbc4b0c489197726b8c
NegativeOnBlock <79171729+NegativeOnBlock@users.noreply.github.com>
2021-03-21 19:44:19 -0500

    Built the grid!
    The grid partially works and I've successfully fixed the page not found issue.

--- designs.js
+++ designs.js
@@ -5,34 +5,41 @@ let selectedSize = document.getElementById('sizePicker');
 
 // When size is submitted by the user, call makeGrid()
 selectedSize.addEventListener('submit', function(event) {
-	event.preventDefault
+	event.preventDefault()
 	return makeGrid()
-});
+
+}); 
 
 // A button to clear the canvas
 let clearBtn = document.querySelector('input[type="button"]');
 
 clearBtn.addEventListener('button', function() {
-
+	
 });
 
-// Creates the grid of the canvas
+// Creates the grid of the canvas 
 function makeGrid() {
 	let canvas = document.getElementById('pixelCanvas');
-	let gridWidth = document.getElementById('inputWidth');
-	let gridHeight = document.getElementById('inputHeight');
+	let gridWidth = document.getElementById('inputWidth').value;
+	let gridHeight = document.getElementById('inputHeight').value;
 	let tableBody = document.createElement('tbody');
-
+	
 	for (let x = 0; x < gridWidth; x++) {
 			let gridRow = document.createElement('tr');
-
+			tableBody.appendChild(gridRow);
+	
 			for (let y = 0; y < gridHeight; y++) {
-					let gridCell = document.createElement('td');
+					let gridCell = document.createElement('td'); 
 					gridRow.appendChild(gridCell);
-			}
-					gridCell.addEventListener('mousedown', function() {
-		        			selectedColor.appendChild(gridCell).style.backgroundColor = color;
-		      			});
-    	} 
-}
+		
+					tableBody.appendChild(gridRow);
 			
+					gridCell.addEventListener('mousedown', function(event) {
+		        			event.target.style.backgroundColor = selectedColor.value;
+		      			});
+			} 
+		
+   	} 
+	canvas.appendChild(tableBody);
+	
+}
