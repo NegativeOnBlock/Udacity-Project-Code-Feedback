28a4bc96649b2eac5e724957a59d7f624a76d0b8
NegativeOnBlock <79171729+NegativeOnBlock@users.noreply.github.com>
2021-03-25 10:02:18 -0500

    The clear button is now functional!

--- designs.js
+++ designs.js
@@ -2,31 +2,34 @@
 let selectedColor = document.getElementById('colorPicker');
 // Select size input
 let selectedSize = document.getElementById('sizePicker');
+// Grid values
+let canvas = document.getElementById('pixelCanvas');
+let gridWidth = document.getElementById('inputWidth').value;
+let gridHeight = document.getElementById('inputHeight').value;
 
 // When size is submitted by the user, call makeGrid()
 selectedSize.addEventListener('submit', function(event) {
 	event.preventDefault()
 	return makeGrid()
-
 }); 
 
 // A button to clear the canvas
 let clearBtn = document.querySelector('input[type="button"]');
 
-clearBtn.addEventListener('button', function() {
-	
+clearBtn.addEventListener('click', function() {
+	Array.from(canvas.getElementsByTagName("td")).forEach(cell=>{
+		cell.style.backgroundColor="";
+	})
 });
 
 // Creates the grid of the canvas 
 function makeGrid() {
-	let canvas = document.getElementById('pixelCanvas');
-	let gridWidth = document.getElementById('inputWidth').value;
-	let gridHeight = document.getElementById('inputHeight').value;
+	canvas.innerHTML="";
 	let tableBody = document.createElement('tbody');
 	
 	for (let x = 0; x < gridWidth; x++) {
 			let gridRow = document.createElement('tr');
-			tableBody.appendChild(gridRow);
+					tableBody.appendChild(gridRow);
 	
 			for (let y = 0; y < gridHeight; y++) {
 					let gridCell = document.createElement('td'); 
@@ -40,6 +43,5 @@ function makeGrid() {
 			} 
 		
    	} 
-	canvas.appendChild(tableBody);
-	
+	canvas.appendChild(tableBody);	
 }
